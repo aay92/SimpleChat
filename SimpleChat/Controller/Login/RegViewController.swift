058f80514d1d724a5f333f3e354579182a0899f6
Aleksey <aay92@yandex.ru>
2023-07-19 21:59:22 +0400

    Create new class

--- SimpleChat/Controller/Login/RegViewController.swift
+++ SimpleChat/Controller/Login/RegViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class RegViewController: UIViewController {
     
+    @IBOutlet weak var eamilField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var rePasswordField: UITextField!
+    
+    
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var viewEmail: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var rePasswordView: UIView!
+    
+    
     var delegate: LoginViewControllerDelegate!
-
+    var checkField = CheckField.shared
+    var tapGestureRecognizer = UITapGestureRecognizer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeEditing))
+        mainView.addGestureRecognizer(tapGestureRecognizer)
 
+    }
+    
+    @objc func closeEditing(){
+        self.view.endEditing(true)
     }
     
     @IBAction func closeVCBtn(_ sender: Any) {
