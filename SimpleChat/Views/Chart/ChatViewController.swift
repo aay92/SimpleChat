04930287e58efd0c99c4e998c6ef32de708f559a
Aleksey <aay92@yandex.ru>
2023-08-04 21:12:42 +0400

    Create field chat

---
+++ SimpleChat/Views/Chart/ChatViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ChatViewController.swift
+//  SimpleChat
+//
+//  Created by Aleksey Alyonin on 04.08.2023.
+//
+
+import UIKit
+import MessageKit
+import InputBarAccessoryView ///чтобы работать с кнопкой send в чате
+
+
+///Sender - кто отправляет сообщение
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
+}
+
+///Message - само сообщение
+struct Message: MessageType {
+    var sender: MessageKit.SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKit.MessageKind
+}
+
+class ChatViewController: MessagesViewController {
+
+    let selfSender = Sender(senderId: "1", displayName: "Me")
+    let otherSender = Sender(senderId: "2", displayName: "Alex")
+    var messages = [Message]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date().addingTimeInterval(-11200), kind: .text("Привет")))
+        messages.append(Message(sender: otherSender, messageId: "2", sentDate: Date().addingTimeInterval(-10200), kind: .text("Привет. Как дела?")))
+
+        ///messagesCollectionView есть в классе MessagesViewController и мы можем обращаться к экземпляру данного класса
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self ///делегат кпопки send
+        showMessageTimestampOnSwipeLeft = true ///показывать время по свайпу
+    }
+
+}
+
+//MARK: - MessagesDisplayDelegate, MessagesLayoutDelegate, MessagesDataSource
+extension ChatViewController: MessagesDisplayDelegate, MessagesLayoutDelegate, MessagesDataSource {
+    
+    ///currentSender - тот кто сейчас главный
+    func currentSender() -> MessageKit.SenderType {
+        return selfSender
+    }
+    ///messageForItem - сообщения
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        return messages.count
+    }
+
+}
+
+extension ChatViewController: InputBarAccessoryViewDelegate {
+    ///didPressSendButtonWith - отпрака сообщения
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        messages.append(Message(sender: selfSender, messageId: "124", sentDate: Date(), kind: .text(text))) /// messages.append добавляем сообщение в масси
+        inputBar.inputTextView.text = nil ///отчищаем поле
+        messagesCollectionView.reloadDataAndKeepOffset() /// обновляем коллекциюмм messages.append
+    }
+}
---
+++ SimpleChat/Views/Chart/MessageListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MessageListViewController.swift
+//  SimpleChat
+//
+//  Created by Aleksey Alyonin on 04.08.2023.
+//
+
+import UIKit
+
+class MessageListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+}
+//MARK: - UITableViewDelegate, UITableViewDataSource
+extension MessageListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Alex"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let vc = ChatViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
